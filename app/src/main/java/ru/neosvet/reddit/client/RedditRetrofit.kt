9ab178a7a1a832ca2f82502f873ed09ad1132e0d
Neosvet <neosvet333@gmail.com>
2022-02-23 16:17:59 +0300

    design client

---
+++ app/src/main/java/ru/neosvet/reddit/client/RedditRetrofit.kt
@@ -0,0 +1,30 @@
+package ru.neosvet.reddit.client
+
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RedditRetrofit {
+    private const val URL = "https://www.reddit.com/"
+
+    fun create(): Reddit =
+        Retrofit.Builder()
+            .baseUrl(URL)
+            .client(
+                OkHttpClient.Builder()
+                    .addInterceptor(HttpLoggingInterceptor().apply {
+                        level = HttpLoggingInterceptor.Level.BODY
+                    })
+                    .build()
+            )
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(Reddit::class.java)
+
+    private val gson = GsonBuilder()
+        .create()
+}
